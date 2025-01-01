9b0883c28e1771c8b6a4abd4a14a18d5bdb4b34a
Rahul Kumawat <kumawat30rahul@gmail.com>
2025-01-01 22:53:06 +0530

    chagnes in responsivness of video and added github readme loader

--- src/components/pages/githubProfilePage/index.tsx
+++ src/components/pages/githubProfilePage/index.tsx
@@ -2,21 +2,36 @@ import { useEffect, useState } from "react";
 
 const GithubProfilePage = () => {
   const [profileData, setProfileData] = useState<any>(null);
+  const [loader, setLoader] = useState(true);
 
   const fetchGithubProfile = async () => {
-    const response = await fetch(
-      `https://api.github.com/repos/${"kumawat30rahul"}/${"kumawat30rahul"}/contents/README.md`
-    );
-    const data = await response.json();
-    console.log(data);
-
-    if (data?.content) {
-      // Decode base64 content
-      const decodedContent = atob(data.content).replace(
-        /👋|🌟|💡|🔍|🤝|📚|💬/g,
-        (emoji) => `<span>${emoji}</span>`
+    setLoader(true);
+    try {
+      const response = await fetch(
+        `https://api.github.com/repos/${"kumawat30rahul"}/${"kumawat30rahul"}/contents/README.md`,
+        {
+          headers: {
+            Authorization: `token ${import.meta.env.VITE_GITHUB_REPO_TOKEN}`,
+          },
+        }
       );
-      setProfileData(decodedContent);
+      const data = await response.json();
+      if (Number(data?.status) > 400) {
+        setLoader(false);
+        return;
+      }
+      if (data?.content) {
+        // Decode base64 content
+        const decodedContent = atob(data.content).replace(
+          /👋|🌟|💡|🔍|🤝|📚|💬/g,
+          (emoji) => `<span>${emoji}</span>`
+        );
+        setProfileData(decodedContent);
+        setLoader(false);
+      }
+    } catch (error) {
+      console.error("Error fetching Github profile:", error);
+      setLoader(false);
     }
   };
 
@@ -26,7 +41,18 @@ const GithubProfilePage = () => {
 
   return (
     <div className="mt-[100px]">
-      <div dangerouslySetInnerHTML={{ __html: profileData }} />
+      {!profileData && !loader && (
+        <div className="flex items-center justify-center w-full">
+          <h2 className="text-2xl font-bold">No data found</h2>
+        </div>
+      )}
+      {loader ? (
+        <div className="mt-[100px] flex items-center justify-center w-full">
+          <div className="loader"></div>
+        </div>
+      ) : (
+        <div dangerouslySetInnerHTML={{ __html: profileData }} />
+      )}
     </div>
   );
 };
